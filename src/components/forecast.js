80b32a628641265d8330c1cb1c52b710c76d30a4
duch3201 <56601374+duch3201@users.noreply.github.com>
2022-11-02 18:39:49 +0100

    initial commit

---
+++ src/components/forecast.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Forecast() {
+   // fetch("https://my-json-server.typicode.com/duch3201/samplejsapi/responce")
+    //.then(response => response.json())
+    //.then(data => console.log(data));
+
+    //const ddata=data.map{
+
+    //}
+
+    console.log("test")
+
+}
