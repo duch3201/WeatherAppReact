0a84d6195d2246ed5d37c626bb749d3e8330410b
_Ghost_ <56601374+duch3201@users.noreply.github.com>
2022-11-04 13:18:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import './App.css';
 
 var today = new Date();
 var time = today.getHours() + ":" + today.getMinutes()
-//let city = localStorage.getItem('city')
+let city = localStorage.getItem('city')
 
 
 class Topbar extends React.Component {
   render() {
     return <h1 class="text-2xl pt-4 pb-5 self-center">{time} · {this.props.name}</h1>;
+  }
+}
+
+class requesttoapi extends React.Component {
+  render() {
+    return (
+      <h1>requesttoapi</h1>
+    )
   }
 }
 
@@ -19,7 +27,7 @@ class Forecast extends React.Component {
   async componentDidMount() {
     let city = localStorage.getItem('city');
     let lang = localStorage.getItem('lang');
-   // let test = "http://localhost:5000/get-weather/"+city+"/"+lang
+    let test = "http://localhost:5000/get-weather/"+city+"/"+lang
     let apiurl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+process.env.APIKEY+"&lang=en&units=metric ";
     // GET request using fetch with async/await
     fetch(apiurl)
@@ -58,7 +66,17 @@ class Forecast extends React.Component {
                 <h1 id="s" class="text-xl">status</h1>
             </div>
         )
+    }
 
+    return (
+      <div>
+        <h1 id="temp" class="text-9xl">{weather.temp}</h1>
+ 
+        <h1 id="fl" class="text-xl pt-5 pb-2">feels like</h1>
+        <h1 id="ws" class="text-xl pb-2">wind speed</h1>
+        <h1 id="s" class="text-xl">status</h1>
+      </div>
+    );
 
   }
 }
@@ -68,6 +86,7 @@ class Forecast extends React.Component {
 
 const element = <Topbar name={city} />;
 const weather = <Forecast name={"data"} />;
+const test = <requesttoapi/>
 
 function App() {
   return (
